eae993db0c1273bf62c48bc38ca8bfe50bfd7bd3
JeanFerreira91 <jeanferreira91@hotmail.com>
2022-05-14 23:25:46 +0100

    testing

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+gunicorn Portfolio2022.wsgi:application
